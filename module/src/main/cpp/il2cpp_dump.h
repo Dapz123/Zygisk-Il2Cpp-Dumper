fbf6e801d2fc9aabcba754669f4ecf6b8503bc92
Bangdap <134949593+Dapz123@users.noreply.github.com>
2024-06-18 14:35:49 +0700

    New function

--- module/src/main/cpp/il2cpp_dump.h
+++ module/src/main/cpp/il2cpp_dump.h
@@ -7,6 +7,9 @@
 
 void il2cpp_api_init(void *handle);
 
-void il2cpp_dump(const char *outDir);
+void il2cpp_dump_all(const char *outDir);
+void il2cpp_dump_method(const char *outDir);
+void il2cpp_dump_property(const char *outDir);
+void il2cpp_field_propert(const char *outDir);
 
 #endif //ZYGISK_IL2CPPDUMPER_IL2CPP_DUMP_H
