fdd4ee0d9e4ceaf6f34a320572d0c2cca9122c58
Bangdap <134949593+Dapz123@users.noreply.github.com>
2024-06-18 09:19:52 +0700

    Update method dump

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -38,12 +38,11 @@ std::string get_method_modifier(uint32_t flags) {
     std::stringstream outPut;
     auto access = flags & METHOD_ATTRIBUTE_MEMBER_ACCESS_MASK;
     switch (access) {
+        /*
         case METHOD_ATTRIBUTE_PRIVATE:
             outPut << "private ";
             break;
-        case METHOD_ATTRIBUTE_PUBLIC:
-            outPut << "public ";
-            break;
+        
         case METHOD_ATTRIBUTE_FAMILY:
             outPut << "protected ";
             break;
@@ -53,6 +52,10 @@ std::string get_method_modifier(uint32_t flags) {
             break;
         case METHOD_ATTRIBUTE_FAM_OR_ASSEM:
             outPut << "protected internal ";
+            break;
+        */
+        case METHOD_ATTRIBUTE_PUBLIC:
+            outPut << "public ";
             break;
     }
     if (flags & METHOD_ATTRIBUTE_STATIC) {
@@ -88,34 +91,29 @@ bool _il2cpp_type_is_byref(const Il2CppType *type) {
     return byref;
 }
 
+// Dump Method
 std::string dump_method(Il2CppClass *klass) {
     std::stringstream outPut;
-    outPut << "\nMethods\n";
     void *iter = nullptr;
     while (auto method = il2cpp_class_get_methods(klass, &iter)) {
-        //TODO attribute
+        // METHOD OFFSET
         if (method->methodPointer) {
             outPut << "0x";
             outPut << std::hex << (uint64_t) method->methodPointer - il2cpp_base;
-            // outPut << " VA: 0x";
-            // outPut << std::hex << (uint64_t) method->methodPointer;
         } else {
-            outPut << " * ";
+            outPut << "×";
         }
-        /*if (method->slot != 65535) {
-            outPut << " Slot: " << std::dec << method->slot;
-        }*/
         outPut << " ";
         uint32_t iflags = 0;
         auto flags = il2cpp_method_get_flags(method, &iflags);
-        outPut << get_method_modifier(flags);
+        outPut << get_method_modifier(flags) << " get_method_modifier(flags) ";
         //TODO genericContainerIndex
         auto return_type = il2cpp_method_get_return_type(method);
         if (_il2cpp_type_is_byref(return_type)) {
-            outPut << "ref ";
+            outPut << "ref " << " _il2cpp_type_is_byref(return_type) ";
         }
         auto return_class = il2cpp_class_from_type(return_type);
-        outPut << il2cpp_class_get_name(return_class) << " " << il2cpp_method_get_name(method)
+        outPut << il2cpp_class_get_name(return_class) << " il2cpp_class_get_name(return_class) " << il2cpp_method_get_name(method) << " il2cpp_method_get_name(method) "
                << "(";
         auto param_count = il2cpp_method_get_param_count(method);
         for (int i = 0; i < param_count; ++i) {
@@ -145,7 +143,7 @@ std::string dump_method(Il2CppClass *klass) {
         if (param_count > 0) {
             outPut.seekp(-2, outPut.cur);
         }
-        outPut << ") { }\n";
+        outPut << ")\n";
         //TODO GenericInstMethod
     }
     return outPut.str();
