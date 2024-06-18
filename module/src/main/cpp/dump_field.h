9ac5bc08ef4578fb59cc8400d510a405dd3f347e
Bangdap <134949593+Dapz123@users.noreply.github.com>
2024-06-18 14:25:19 +0700

    Create dump_field.h

---
+++ module/src/main/cpp/dump_field.h
@@ -0,0 +1,56 @@
+#ifndef FIELD
+#define FIELD
+
+std::string dump_field(Il2CppClass *klass) {
+    std::stringstream outPut;
+    outPut << "\nFields\n";
+    auto is_enum = il2cpp_class_is_enum(klass);
+    void *iter = nullptr;
+    while (auto field = il2cpp_class_get_fields(klass, &iter)) {
+        //TODO attribute
+        outPut << "\t";
+        auto attrs = il2cpp_field_get_flags(field);
+        auto access = attrs & FIELD_ATTRIBUTE_FIELD_ACCESS_MASK;
+        switch (access) {
+            case FIELD_ATTRIBUTE_PRIVATE:
+                outPut << "private ";
+                break;
+            case FIELD_ATTRIBUTE_PUBLIC:
+                outPut << "public ";
+                break;
+            case FIELD_ATTRIBUTE_FAMILY:
+                outPut << "protected ";
+                break;
+            case FIELD_ATTRIBUTE_ASSEMBLY:
+            case FIELD_ATTRIBUTE_FAM_AND_ASSEM:
+                outPut << "internal ";
+                break;
+            case FIELD_ATTRIBUTE_FAM_OR_ASSEM:
+                outPut << "protected internal ";
+                break;
+        }
+        if (attrs & FIELD_ATTRIBUTE_LITERAL) {
+            outPut << "const ";
+        } else {
+            if (attrs & FIELD_ATTRIBUTE_STATIC) {
+                outPut << "static ";
+            }
+            if (attrs & FIELD_ATTRIBUTE_INIT_ONLY) {
+                outPut << "readonly ";
+            }
+        }
+        auto field_type = il2cpp_field_get_type(field);
+        auto field_class = il2cpp_class_from_type(field_type);
+        outPut << il2cpp_class_get_name(field_class) << " " << il2cpp_field_get_name(field);
+        //TODO 获取构造函数初始化后的字段值
+        if (attrs & FIELD_ATTRIBUTE_LITERAL && is_enum) {
+            uint64_t val = 0;
+            il2cpp_field_static_get_value(field, &val);
+            outPut << " = " << std::dec << val;
+        }
+        outPut << "; // 0x" << std::hex << il2cpp_field_get_offset(field) << "\n";
+    }
+    return outPut.str();
+}
+
+#endif
